8c7222af3fa53965c871d39ed60629aa0d471a29
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-08-28 22:53:32 -0500

    added style bg color and appended search in java

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log(searchesSaved);
 for (var i = 0; i < searchesSaved.length; i++) {
     makelist(savedSearches)
 }
-var apiKey = "trilogy";
+var apiKey = "6441e6e31c53654c07024f7ffc57d21a";
 
 
 
@@ -16,18 +16,39 @@ $("body").prepend(title);
 
 $(document).ready(function () {
 
+    var newSearch = $("#new-info");
+
     $("#button").on("click", function () {
         event.preventDefault()
         console.log("working")
 
-        var userSearch = $("#search").val()
+
+        var userSearch = $("#search").val();
         console.log(userSearch)
 
         searchesSaved.push(userSearch)
 
         window.localStorage.setItem(userSearch, JSON.stringify(searchesSaved));
-        weatherInfo(searchesSaved)
+        weatherInfo(userSearch);
 
+        $(newSearch).append(userSearch + ("<br>"));
     });
 
+
 });
+
+function weatherInfo(userSearch) {
+    var weatherTemp = $("#weather-conditions");
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + userSearch + "&units=imperial&appid=" + apiKey;
+
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        $(weatherTemp).append(JSON.stringify(response));
+
+    })
+
+}
